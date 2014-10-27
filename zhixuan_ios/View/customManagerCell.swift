9259aadda2edeca4c79f95cdc61cb45b303e026d
simplejoy <lantian-lz@163.com>
2014-10-27 20:04:32 +0800

    营业部页面构造完成，需要微调

--- zhixuan_ios/View/customManagerCell.swift
+++ zhixuan_ios/View/customManagerCell.swift
@@ -10,12 +10,23 @@ import UIKit
 
 
 class CustomManagerCell: UITableViewCell {
-    //营业部表格自定义
+    //客户经理表格自定义
     @IBOutlet weak var cmImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var telLabel: UILabel!
     @IBOutlet weak var qqLabel: UILabel!
     @IBOutlet weak var vipInfoLabel: UILabel!
     @IBOutlet weak var companyLabel: UILabel!
+    
+}
+
+
+class CustomManagerOfDepartmentCell: UITableViewCell {
+    //营业部客户经理表格自定义
+    @IBOutlet weak var cmImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var telLabel: UILabel!
+    @IBOutlet weak var qqLabel: UILabel!
+    @IBOutlet weak var vipInfoLabel: UILabel!
     
 }
--- zhixuan_ios/ViewController/DepartmentDetailController.swift
+++ zhixuan_ios/ViewController/DepartmentDetailController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class DepartmentDetailController: UIViewController {
+class DepartmentDetailController: UIViewController, UITableViewDataSource, UITableViewDelegate, HttpRequestProtocol {
     @IBOutlet weak var labelDepartmentName: UILabel!
     @IBOutlet weak var imageViewDepartment: UIImageView!
     @IBOutlet weak var labelCompanyName: UILabel!
     @IBOutlet weak var labelTel: UILabel!
     @IBOutlet weak var labelAddr: UILabel!
     @IBOutlet weak var textDes: UITextView!
-    
+    @IBOutlet weak var cmOfDepartmentTable: UITableView!
+    @IBOutlet weak var cmSrollView: UIScrollView!
     
     var img:UIImage!
     var departmentObj:NSDictionary?
     var httpRequest = HttpRequest()
+    var cmObjs = NSMutableArray()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +42,12 @@ class DepartmentDetailController: UIViewController {
 
         var navButtonItem = UIBarButtonItem(title: "返回", style: UIBarButtonItemStyle.Plain, target: self, action: NSSelectorFromString("backToDepartmentList:"))
         self.navigationItem.setLeftBarButtonItem(navButtonItem, animated: true)
+        
+        self.cmOfDepartmentTable.tableFooterView = UIView(frame: CGRectZero)
+        
+        let department_id = departmentObj!["id"] as Int
+        self.httpRequest.delegate = self
+        self.httpRequest.getResultsWithJson("\(MAINDOMAIN)/kaihu/api_get_custom_manager_list_of_department?department_id=\(department_id)")
     }
     
     override func didReceiveMemoryWarning() {
@@ -48,6 +57,60 @@ class DepartmentDetailController: UIViewController {
     
     func backToDepartmentList(sender: UIBarButtonItem) {
         self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return self.cmObjs.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        var cell = tableView.dequeueReusableCellWithIdentifier("customManagerOfDepartment") as CustomManagerOfDepartmentCell
+        let rowData:NSDictionary = self.cmObjs[indexPath.row] as NSDictionary
+        
+        cell.nameLabel?.text = rowData["nick"] as? String
+        cell.telLabel?.text = rowData["mobile"] as? String
+        cell.qqLabel?.text = rowData["qq"] as? String
+        cell.vipInfoLabel?.text = rowData["vip_info"] as? String
+        
+        let img = httpRequest.getImage(rowData["img"] as String)
+        cell.cmImageView?.image = img
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath){
+        cell.layer.transform = CATransform3DMakeScale(0.1, 0.1, 1)
+        UIView.animateWithDuration(0.25, animations: {
+            cell.layer.transform = CATransform3DMakeScale(1, 1, 1)
+        })
+    }
+    
+    
+    func didRecieveResults(results:NSDictionary){
+        self.cmObjs = results["custom_managers"] as NSMutableArray
+        if(self.cmObjs.count == 0){
+//            self.cmOfDepartmentTable.hidden = true
+        }else{
+//            self.cmOfDepartmentTable.hidden = false
+        }
+        
+        
+        println(self.cmOfDepartmentTable.frame)
+        let height = CGFloat(65 * self.cmObjs.count)
+//        self.cmOfDepartmentTable.frame = CGRectMake(16, 385, 350, 600)
+//        println(self.cmOfDepartmentTable.frame)
+        
+        self.cmSrollView.contentSize = CGSize(width: 400, height: 1600)
+        
+        self.cmOfDepartmentTable.reloadData()
+        println(self.cmOfDepartmentTable.contentSize)
+        
+        println(self.cmOfDepartmentTable)
+        self.cmOfDepartmentTable.frame = CGRectMake(16, 385, 350, 248)
+        self.cmOfDepartmentTable.reloadData()
+        println(self.cmOfDepartmentTable)
+        
     }
     
     
